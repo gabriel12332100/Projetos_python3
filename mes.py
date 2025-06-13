196a6934c6bc300c3e02636ad8cd23a25f91a70c
gabriel12332100 <145794108+gabriel12332100@users.noreply.github.com>
2025-06-12 23:06:46 -0300

    Add files via upload

---
+++ mes.py
@@ -0,0 +1,10 @@
+
+month_number = int(input())
+
+
+months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+print(months[month_number - 1])
